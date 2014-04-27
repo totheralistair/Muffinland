7c1ed4cb313f89593b2eb78dbe131852ad248667
totheralistair <totheralistair@aol.com>
2014-04-27 09:32:32 -0400

    broken code trying to get views to work
    come back to this later

--- test/test_muffinland2.rb
+++ test/test_muffinland2.rb
@@ -2,7 +2,7 @@ require 'rack/test'
 require 'rspec/expectations'
 require 'test/unit'
 
-require_relative '../src/muffinland1.rb'
+require_relative '../src/muffinland.rb'
 
 class TestRequests < Test::Unit::TestCase
   include Rack::Test::Methods
@@ -22,18 +22,15 @@ class TestRequests < Test::Unit::TestCase
 #=================================================
   def test_00_get_without_server
     app = Muffinland.new
-    run_without_server( app, "GET", '/blarg?A=aa&B=bb', "getKey=getValue").body.should ==
-        "Nice GET there. Page requested = /blarg. Params = {\"getKey\"=>\"getValue\", \"A\"=>\"aa\", \"B\"=>\"bb\"}. Bests. Alistair."
     run_without_server( app, "POST", '/ook', "postKey=postValue" ).body.should ==
         "Got that POST, baby. Page requested = /ook. Params = {\"postKey\"=>\"postValue\"}. "
+    run_without_server( app, "GET", '/blarg?A=aa&B=bb', "getKey=getValue").body.should ==
+        "Nice GET there. Page requested = /blarg. Params = {\"getKey\"=>\"getValue\", \"A\"=>\"aa\", \"B\"=>\"bb\"}. Bests. Alistair."
+    run_without_server( app, "GET", '/helloalistair').body.should ==
+        "hello, Alistair"
+    run_without_server( app, "GET", '/0').body.should ==
+        "should be 0, Alistair"
   end
 
-  def test_01_RUN_with_server
-    app = Muffinland.new
-    run_with_server( app, "GET", '/sweetie?A=a&B=b', "getKey=getValue").body.should ==
-        "Nice GET there. Page requested = /sweetie. Params = {\"getKey\"=>\"getValue\", \"A\"=>\"a\", \"B\"=>\"b\"}. Bests. Alistair."
-    run_with_server( app, "POST", '/narf', "postKey=postValue").body.should ==
-        "Got that POST, baby. Page requested = /narf. Params = {\"postKey\"=>\"postValue\"}. "
-  end
 end
 
