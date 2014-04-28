fa1d11537b854d48cd38c2d2b5ed3b0008e02409
totheralistair <totheralistair@aol.com>
2014-04-27 21:51:49 -0400

    first use of Erubis to put out a vew/template
    (ERB didn't work after several hours of trying, Erubis worked straight
    away, so Erubis it is for now).
    Not clean or anything, but keeping track of my code as this is the first
    one that worked with a view.

--- src/Muffinland.rb
+++ src/Muffinland.rb
@@ -4,10 +4,10 @@
 require 'rack'
 require 'erb'
 require 'sinatra'
-
+require 'erubis'
 
 class Muffinland < Sinatra::Base
-
+attr :path, :params
   def call(env)
     request  = Rack::Request.new(env)
     if request.get? then
@@ -18,39 +18,52 @@ class Muffinland < Sinatra::Base
   end
 end
 
-def handle_post( request ) # expect Rack::Request, return Rack::Response
-  @myPosts ||= Array.new
-  @myPosts.push(request)
-
-  params = request.params
-  pathinfo = request.path_info
-
-  response = Rack::Response.new
-  response['Content-Type'] = 'text/html'
-  response.write "Got that POST, baby. "
-  response.write "Page requested = #{pathinfo}. "
-  response.write "Params = #{params}. "
-  response.finish
+def get_erb( pathToViews, viewfilename )
+  fn = pathToViews + viewfilename
+  ERB.new(File.open( fn, 'r').read)
 end
 
 
-
 def handle_get( request )
+  pathToViews = "../src/views/"
+
   params = request.params
   path = request.path
 
   response = Rack::Response.new
-
-#   erbOut = erb :test1
-
-  erbOut = erb :view01_justaview
   response['Content-Type'] = 'text/html'
-  response.write "it's a GET. "
-  response.write "Page requested = #{path}. "
-  response.write "Params = #{params}. "
-  response.write erbOut
+  response.write "it's a GET."
+  response.write "\<BR\>"
+  response.write "Page requested = #{path}."
+  response.write "\<BR\>"
+  response.write "Params = #{params}."
+  response.write "\<BR\>"
+
+#  day = "wednesday"
+#  day_template = "Today is <%= day %>."
+  viewfilename = "simpleGET.erb"
+  fn = pathToViews + viewfilename
+  eruby = Erubis::Eruby.new(File.open( fn, 'r').read)
+  out = eruby.result(:path=>path, :params=>params)
+  response.write out
+#  erb = ERB.new(File.open( fn, 'r').read)
+#  response.write erb.result
+
+
+=begin
+  erb = ERB.new(simple_template)
+  puts output = erb.result()
+#  erb = get_erb( pathToViews, "simpleGET.erb")
+  viewfilename = "simpleGET.erb"
+  fn = pathToViews + viewfilename
+  erb = ERB.new(File.open( fn, 'r').read)
+  response.write erb.result
+=end
+
+  response.write "\<BR\>"
 #  response.write outFor(path, params)
   response.write "Bests. Alistair."
+  response.write "\<BR\>"
   response.finish
 
 end
@@ -70,6 +83,21 @@ end
 
 
 
+#===================================================
+def handle_post( request ) # expect Rack::Request, return Rack::Response
+  @myPosts ||= Array.new
+  @myPosts.push(request)
+
+  params = request.params
+  pathinfo = request.path_info
+
+  response = Rack::Response.new
+  response['Content-Type'] = 'text/html'
+  response.write "Got that POST, baby. "
+  response.write "Page requested = #{pathinfo}. "
+  response.write "Params = #{params}. "
+  response.finish
+end
 
 
 
